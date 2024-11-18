39113eb6f1ec98cf0f188561c1a022a44b2165cc
Jacob Hyatt <31117979+thejacobhyatt@users.noreply.github.com>
2024-11-17 21:29:01 -0500

    rms_sql script

--- RMS/settings.py
+++ RMS/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'RMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rms',
+        'NAME': 'researchdb',
         'USER': 'django',
         'PASSWORD': 'mysecretpassword',
         'HOST': 'localhost',
--- rms_app/urls.py
+++ rms_app/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('add_fake_data/', views.add_fake_data, name='add_fake_data'),
-    path('show_fake_data/', views.show_fake_data, name='show_fake_data'),
 ]
